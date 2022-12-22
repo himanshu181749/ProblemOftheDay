ac16d088db1d5df9172bc8f4ad59fbc7af278038
Himanshu Gupta <75875283+himanshu181749@users.noreply.github.com>
2022-12-22 16:30:42 +0530

    Create 22cDecGFG.cpp

---
+++ 22cDecGFG.cpp
@@ -0,0 +1,17 @@
+class Solution{
+    #define ll long long
+public:
+    //Function to count subarrays with sum equal to 0.
+    long long int findSubarray(vector<long long int> &arr, int n ) {
+        long long int result = 0;
+        map<long long int, int> freq;
+        long long int sum = 0;
+        freq[0]++;
+        for(int i = 1; i <= n; i++){
+            sum += arr[i-1];
+            freq[sum]++;
+            result += freq[sum] - 1;
+        }
+        return result;
+    }
+};
